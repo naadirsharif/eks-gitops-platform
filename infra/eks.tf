7f58bf3c40e0274c126dd5f3c273f7f5d85eaac3
naadirlab <sharifnaadir@gmail.com>
2026-05-23 21:23:09 +0200

    feat(eks): public and private subnet ids

--- infra/eks.tf
+++ infra/eks.tf
@@ -2,5 +2,15 @@ module "eks" {
     source = "terraform-aws-modules/eks/aws"
     
     cluster_name = local.name 
-    cluster_version = "1.23"
+    cluster_version = "1.28"
+
+    cluster_endpoint_public_access = true
+    cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"] 
+
+    enable_irsa = true 
+
+    vpc_id = module.vpc.id
+    subnet_ids = module.vpc.private_subnets 
+    control_plane_subnet_ids = module.vpc.public_subnets
+
 }
