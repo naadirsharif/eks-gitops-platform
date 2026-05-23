c2df49a87b6aef1b17a5c46a3f0ffcf0f5dff488
naadirlab <sharifnaadir@gmail.com>
2026-05-23 13:47:36 +0200

    feat(locals): add project locals for name, domain, region and tags

---
+++ infra/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  name = "eks-lab"
+  domain = "lab.nashar.dev"
+  region = "eu-central-1" # Frankfurt region
+
+
+  tags = {
+    Environment = "sandbox"
+    Project = "eks-gitops-platform"
+    Owner = "Naadir"
+  }
+}
