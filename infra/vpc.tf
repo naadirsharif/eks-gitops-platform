63f9be2b04b92e106142769a4d48c7000ac275b1
naadirlab <sharifnaadir@gmail.com>
2026-05-23 14:01:37 +0200

    feat(vpc): add private subnets

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -8,6 +8,11 @@ module "vpc" {
         "${local.region}-a",
         "${local.region}-b",
         "${local.region}-c"
-        ]
+    ]
 
+    private_subnets = [
+        "10.0.1.0/24", 
+        "10.0.2.0/24",
+        "10.0.3.0/24"
+    ]
 }
