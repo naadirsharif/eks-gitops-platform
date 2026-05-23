701067542e7076534ce13775f87ed1eca8eeb160
naadirlab <sharifnaadir@gmail.com>
2026-05-23 13:55:22 +0200

    feat(vpc): add availability zones to vpc module

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,4 +1,13 @@
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
     version = "5.9.0"
+
+    name = local.name
+
+    azs =[
+        "${local.region}-a",
+        "${local.region}-b",
+        "${local.region}-c"
+        ]
+
 }
