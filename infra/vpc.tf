8002f5af1e9fcf1271f7a1dee5cd291bd8b64bab
naadirlab <sharifnaadir@gmail.com>
2026-05-23 14:07:16 +0200

    feat(vpc): add public subnets

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -4,6 +4,8 @@ module "vpc" {
 
     name = local.name
 
+    cidr = "10.0.0.0/16"
+
     azs =[
         "${local.region}-a",
         "${local.region}-b",
@@ -14,5 +16,11 @@ module "vpc" {
         "10.0.1.0/24", 
         "10.0.2.0/24",
         "10.0.3.0/24"
+    ]
+
+    public_subnets = [
+        "10.0.4.0/24", 
+        "10.0.5.0/24",
+        "10.0.6.0/24"
     ]
 }
