c6f2978d19bcf3cd4d223293facc27efef871421
naadirlab <sharifnaadir@gmail.com>
2026-05-24 11:04:03 +0200

    refactor(eks): remove node group defaults

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,11 +1,11 @@
 module "eks" {
     source = "terraform-aws-modules/eks/aws"
     
-    cluster_name = local.name 
-    cluster_version = "1.28"
+    name = local.name 
+    kubernetes_version = "1.28"
 
-    cluster_endpoint_public_access = true
-    cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"] 
+    endpoint_public_access = true
+    endpoint_public_access_cidrs = ["0.0.0.0/0"] 
 
     enable_irsa = true 
 
@@ -13,13 +13,9 @@ module "eks" {
     subnet_ids = module.vpc.private_subnets 
     control_plane_subnet_ids = module.vpc.public_subnets
 
-    eks_managed_node_group_defaults = {
+    eks_managed_node_groups = {
         disk_size = 50
         instance_types = ["t3a.large", "t3.large"]
-    }
-
-    eks_managed_node_groups = {
-        default = {}
     }
     tags = local.tags
 }
