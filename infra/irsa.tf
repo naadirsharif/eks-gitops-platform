fc66397a15213c261ee755b216a4ab057c84675f
naadirlab <sharifnaadir@gmail.com>
2026-05-24 12:13:01 +0200

    feat(irsa): add external DNS IRSA

--- infra/irsa.tf
+++ infra/irsa.tf
@@ -1,10 +1,11 @@
- # Cert manager IRSA (IAM roles for service accounts)
+ ## Cert manager IRSA (IAM roles for service accounts)
+    ## -> Allows cert-manager to create DNS records in Route53 for SSL validation
 
  module "cert_manager_irsa_role" {
     source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
     version = "6.6.0"
     
-    name = "cert_manager"
+    name = "cert-manager"
     attach_cert_manager_policy = true 
     cert_manager_hosted_zone_arns = ["arn:aws:route53:::/hostedzone/Z00208033796D8OPHFLPL"]
 
@@ -14,6 +15,34 @@
             namespace_service_accounts = ["cert-manager:cert-manager"]
         }
     }
+    
     tags = local.tags
  }
+
+## External DNS IRSA
+    ## # -> Automatically creates Route53 DNS records when app is deployed
+
+module "external_dns_irsa_role" {
+     source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
+     version = "6.6.0"
+
+     name = "external-dns"
+     attach_external_dns_policy = true
+     external_dns_hosted_zone_arns = ["arn:aws:route53:::/hostedzone/Z00208033796D8OPHFLPL"]
+
+     oidc_providers = {
+        eks = {
+            provider_arn = module.eks.oidc_provider_arn
+            namespace_service_accounts = ["external-dns:external-dns"]
+        }
+     }
+
+     tags = local.tags
+}
+
+
+
+
+
+
  
