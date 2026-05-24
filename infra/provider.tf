79a5c48953367acec0c33bac444b3cdcd2ba2880
naadirlab <sharifnaadir@gmail.com>
2026-05-24 12:24:58 +0200

    feat(provider): add helm provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,10 +7,20 @@ terraform {
   }
 
   required_version = ">=1.0"
+
   required_providers {
     aws = {
         source = "hashicorp/aws"
         version = ">= 4.0"
     }
+    helm = {
+        source = "hashicorp/helm"
+        version = ">= 2.6"
+    }
   }
+ } 
+
+provider "aws" {
+    region = "eu-central-1"
 }
+
