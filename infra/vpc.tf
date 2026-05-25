e81ff8e8dc31107baa98c0507b33e5d229148e0e
naadirlab <sharifnaadir@gmail.com>
2026-05-25 19:12:19 +0200

    chore(vpc): set map_public_ip_on_launch to true

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "vpc" {
   cidr_block       = var.vpc_cidr_block
-  instance_tenancy = "default"
 
   tags = merge(local.tags, { Name = "${local.name_prefix}-vpc" })
 }
@@ -16,12 +15,14 @@ resource "aws_subnet" "private_subnets" {
   tags = merge(local.tags, { Name = "${local.name_prefix}-private-${each.key}" })
 }
 
+
 resource "aws_subnet" "public_subnets" {
   for_each = local.public_subnet_cidrs
 
   vpc_id            = aws_vpc.vpc.id
   availability_zone = each.key
   cidr_block        = each.value
+  map_public_ip_on_launch = true
 
   tags = merge(local.tags, { Name = "${local.name_prefix}-public-${each.key}" })
 }
