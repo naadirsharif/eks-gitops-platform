9fcb4e7ed0d3888ee1910f29879e2b6435227d27
naadirlab <sharifnaadir@gmail.com>
2026-05-25 11:45:35 +0200

    style(terraform): format eks and provider files with terraform fmt

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "eks-tfstate-lab-bucket"
-    key    = "eks-gitops-platform"
-    region = "eu-central-1"
-    encrypt = true
+    bucket       = "eks-tfstate-lab-bucket"
+    key          = "eks-gitops-platform"
+    region       = "eu-central-1"
+    encrypt      = true
     use_lockfile = true
   }
 
@@ -11,17 +11,17 @@ terraform {
 
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 4.0"
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
     helm = {
-        source = "hashicorp/helm"
-        version = ">= 2.6"
+      source  = "hashicorp/helm"
+      version = ">= 2.6"
     }
   }
- } 
+}
 
 provider "aws" {
-    region = "eu-central-1"
+  region = "eu-central-1"
 }
 
