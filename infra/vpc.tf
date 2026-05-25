2fc97fca7c151364a2afa5faccc2e5e5867f0c77
naadirlab <sharifnaadir@gmail.com>
2026-05-25 19:51:12 +0200

    feat(vpc): add igw and ngw

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -26,3 +26,18 @@ resource "aws_subnet" "public_subnets" {
 
   tags = merge(local.tags, { Name = "${local.name_prefix}-public-${each.key}" })
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = merge(local.tags, { Name = "${local.name_prefix}-igw" })
+}
+
+resource "aws_nat_gateway" "ngw" {
+  vpc_id = aws_vpc.vpc.id
+  availability_mode = "regional"
+  connectivity_type = "public"
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = merge(local.tags, { Name = "${local.name_prefix}-ngw" })
+}
