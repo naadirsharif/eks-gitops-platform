153e995b971e81eb849551dc86411d112d1b0527
naadirlab <sharifnaadir@gmail.com>
2026-05-25 20:05:38 +0200

    chore(vpc): enable dns hostnames

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "vpc" {
   cidr_block       = var.vpc_cidr_block
+  enable_dns_hostnames = true 
 
   tags = merge(local.tags, { Name = "${local.name_prefix}-vpc" })
 }
@@ -40,4 +41,38 @@ resource "aws_nat_gateway" "ngw" {
   depends_on = [aws_internet_gateway.igw]
 
   tags = merge(local.tags, { Name = "${local.name_prefix}-ngw" })
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = merge(local.tags, { Name = "${local.name_prefix}-public-rt" })
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw.id
+  }
+  tags = merge(local.tags, { Name = "${local.name_prefix}-private-rt" })
+}
+
+
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
 }
