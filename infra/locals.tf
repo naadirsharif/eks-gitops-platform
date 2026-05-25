0590de652f76643bb5804510cc3b6b9bffb8df71
naadirlab <sharifnaadir@gmail.com>
2026-05-25 18:36:43 +0200

    feat(locals): add dynamic AZs and subnet CIDRs

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,13 +1,28 @@
 locals {
-  name_prefix   = "nashar-eks-gitops"
-  domain = "eks.nashar.dev"
-  region = "eu-central-1" # Frankfurt region
-
+  name_prefix   = "${var.project_name}-${var.environment}"
+  full_domain = "${var.sub_domain}.${var.base_domain}"
+  region = var.region
 
   tags = {
-    Environment = "lab"
-    Project     = "eks-gitops-platform"
-    Owner       = "Naadir"
+    Environment = var.environment
+    Project     = var.project_name
+    Owner       = var.owner
     ManagedBy   = "terraform"
   }
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnet_cidrs = {
+    "${local.azs[0]}" = "10.0.1.0/24"
+    "${local.azs[1]}" = "10.0.2.0/24"
+    "${local.azs[2]}" = "10.0.3.0/24"
+  }
+
+  public_subnet_cidrs = {
+    "${local.azs[0]}" = "10.0.4.0/24"
+    "${local.azs[1]}" = "10.0.5.0/24"
+    "${local.azs[2]}" = "10.0.6.0/24"
+  }
 }
+
+  
