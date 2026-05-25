c6aa9a44c55b70e53f4e4934a0b522a26c93e9e9
naadirlab <sharifnaadir@gmail.com>
2026-05-25 16:55:26 +0200

    fix(backend): remove locals (not allowed in backend)

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket       = "${local.name_prefix}-tfstate-bucket"
+        bucket       = "nashar-eks-gitops-tfstate-bucket"
         key          = "eks-gitops-platform"
         region       = "eu-central-1"
         encrypt      = true
