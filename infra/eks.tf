8b8f8e890a6c0f8fb70f109aee0351117c5d7ad6
naadirlab <sharifnaadir@gmail.com>
2026-05-28 15:23:25 +0200

    refactor(eks): replace community modules with custom Terraform modules

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,24 +1,75 @@
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+resource "aws_eks_cluster" "cluster" {
+  name = "${local.name_prefix}-cluster"
 
-  name               = local.name
-  kubernetes_version = "1.28"
 
-  endpoint_public_access       = true
-  endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
-  enable_irsa = true ## IAM roles for service accounts
-  ## Enables OIDC provider for pod-level AWS permissions
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
-
-  eks_managed_node_groups = {
-    default = {
-      disk_size      = 50
-      instance_types = ["t3a.large", "t3.large"]
-    }
+  access_config {
+    authentication_mode = "API"
   }
-  tags = local.tags
+
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.35"
+
+  vpc_config {
+    vpc_id = aws_vpc.vpc.id
+    subnet_ids = [ aws_subnet.private_subnets.id ]  # Subnets for workernodes 
+
+    public_access_cidrs = "0.0.0.0/0"
+    endpoint_public_access = true
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]
 }
+
+resource "aws_iam_role" "cluster" {
+  name = local.name_prefix
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+
+
+# module "eks" {
+#   source = "terraform-aws-modules/eks/aws"
+
+#   name               = local.name_prefix
+#   kubernetes_version = "1.28"
+
+#   endpoint_public_access       = true
+#   endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+#   enable_irsa = true ## IAM roles for service accounts
+#   ## Enables OIDC provider for pod-level AWS permissions
+
+#   vpc_id                   = module.vpc.vpc_id
+#   subnet_ids               = module.vpc.private_subnets
+#   control_plane_subnet_ids = module.vpc.public_subnets
+
+#   eks_managed_node_groups = {
+#     default = {
+#       disk_size      = 50
+#       instance_types = ["t3a.large", "t3.large"]
+#     }
+#   }
+#   tags = local.tags
+# }
