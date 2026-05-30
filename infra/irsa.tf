5260066c5fed8302bd00230eaa30466df6f3e01a
naadirlab <sharifnaadir@gmail.com>
2026-05-30 15:16:34 +0200

    feat(irsa): attach policy to external dns role

--- infra/irsa.tf
+++ infra/irsa.tf
@@ -129,7 +129,10 @@ resource "aws_iam_policy" "external_dns" {
   })
 }
 
-
+resource "aws_iam_role_policy_attachment" "external_dns" {
+  role       = aws_iam_role.external_dns.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
 
 
 ## Community module setup (not used atm)
