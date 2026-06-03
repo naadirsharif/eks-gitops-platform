941d9febdc2a6ba73ca86837384362e455f42b1f
naadirlab <sharifnaadir@gmail.com>
2026-06-03 17:52:11 +0200

    refactor(eks): remove eks community module

--- infra/eks.tf
+++ infra/eks.tf
@@ -121,33 +121,3 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node_group.name
 }
-
-
-
-
-
-
-# module "eks" {
-#   source = "terraform-aws-modules/eks/aws"
-
-#   name               = local.name_prefix
-#   kubernetes_version = "1.28"
-
-#   endpoint_public_access       = true
-#   endpoint_public_access_cidrs = ["0.0.0.0/0"]
-
-#   enable_irsa = true ## IAM roles for service accounts
-#   ## Enables OIDC provider for pod-level AWS permissions
-
-#   vpc_id                   = module.vpc.vpc_id
-#   subnet_ids               = module.vpc.private_subnets
-#   control_plane_subnet_ids = module.vpc.public_subnets
-
-#   eks_managed_node_groups = {
-#     default = {
-#       disk_size      = 50
-#       instance_types = ["t3a.large", "t3.large"]
-#     }
-#   }
-#   tags = local.tags
-# }
