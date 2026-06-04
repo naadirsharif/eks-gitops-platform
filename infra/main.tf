388c43e4adf734a38e44fb34fc200fe54d32eded
naadirlab <sharifnaadir@gmail.com>
2026-06-04 17:28:33 +0200

    feat(eks): add eks module

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,18 @@
 module "vpc" {
   source               = "./modules/vpc"
+  region               = var.region
   name_prefix          = local.name_prefix
   tags                 = local.tags
   vpc_cidr_block       = var.vpc_cidr_block
   private_subnet_cidrs = local.private_subnet_cidrs
   public_subnet_cidrs  = local.public_subnet_cidrs
+}
+
+module "eks" {
+  source               = "./modules/eks"
+  region               = var.region
+  name_prefix          = local.name_prefix
+  tags                 = local.tags
+  zone_id              = var.zone_id
+  vpc_cidr_block       = var.vpc_cidr_block
 }
