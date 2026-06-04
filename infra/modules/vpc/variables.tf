8887d370742e6a039f9f02b20ef6685e02899a2e
naadirlab <sharifnaadir@gmail.com>
2026-06-04 16:41:50 +0200

    chore(terraform): add variables for the modules

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+}
+
+variable "vpc_cidr_block" {
+}
+
+variable "owner" {
+}
+
+variable "environment" {
+}
+
+variable "base_domain" {
+}
+
+variable "sub_domain" {
+}
+
+variable "project_name" {
+}
+
+variable "zone_id" {
+}
