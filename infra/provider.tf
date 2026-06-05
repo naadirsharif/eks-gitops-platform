6f3e990625b4b84bd631b595c8c62e4592a32eba
naadirlab <sharifnaadir@gmail.com>
2026-06-05 09:59:14 +0200

    feat(providers): configure Helm provider with EKS cluster connection

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,7 +9,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.6"
+      version = ">= 2.9"
     }
   }
 }
@@ -18,3 +18,14 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
