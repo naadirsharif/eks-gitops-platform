4900d22d6ddbe241f6a9d03af89b5dbfcb26b659
naadirlab <sharifnaadir@gmail.com>
2026-06-05 09:38:03 +0200

    feat(eks): create cluster outputs

---
+++ infra/modules/eks/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_endpoint" {
+  value = aws_eks_cluster.cluster.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
