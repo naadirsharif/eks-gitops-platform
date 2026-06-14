59dd8b387a0bb519dde304d7ac92a43c41be915a
naadirlab <sharifnaadir@gmail.com>
2026-06-14 14:55:20 +0200

    style(terraform): run terraform fmt

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "cluster" {
 
 
   access_config {
-    authentication_mode = "API"
+    authentication_mode                         = "API"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
